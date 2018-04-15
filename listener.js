9b2bb8385d289930244cedcaa3203faf06361006
Bearely Koalified <BearelyKoalified@gmail.com>
2018-04-15 11:35:27 -0400

    barebone server and listener

--- listener.js
+++ listener.js
@@ -4,6 +4,7 @@ var chatServer = net.createServer()
 
 chatServer.on('connection', function(client) {
 	client.write('Hi!\n');
+	client.write('Escape with Ctrl + ]\n');
 
 	client.on('data', function(data) {
 		console.log(data)
